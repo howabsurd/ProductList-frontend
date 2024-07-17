e6bc7df6f585a4827c90ac46b3fab37989c5387b
howabsurd <poptanikrish1@gmail.com>
2024-07-17 23:16:41 +0530

    changed backend auth

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,14 +1,14 @@
 export const backendURI = {
-    "production" : "",
+    "production" : "http://localhost:4700",
     "development" : "http://localhost:4700",
     "testing" : ""
 }
 
 export const authURI = {
-    "production" : "",
+    "production" : "https://product-list-auth.vercel.app",
     "development" : "http://localhost:4800",
     "testing" : ""
 }
 
 
-export const env = "development"
+export const env = "production"
